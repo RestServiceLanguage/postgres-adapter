611e7062c5467fdb19171f6161fb3ba8f7b4257e
Moritz Schramm <moritz.schramm@gmx.de>
2019-12-26 11:53:03 +0100

    Clean Up for rewrite

--- index.js
+++ index.js
@@ -1,50 +1,26 @@
 const errorHandler = require('./lib/errorHandler');
-const { KnexAdapter } = require('@restservicelanguage/database');
+const { DatabaseAdapter } = require('@restservicelanguage/database');
+const pg = require('pg');
 
-module.exports = class PostgresAdapter extends KnexAdapter {
+module.exports = class PostgresAdapter extends DatabaseAdapter {
 
-  constructor({ host, user, password, database }) {
-    super({
-      client: 'pg',
-      connection: {
-        host,
-        user,
-        password,
-        database
-      }
-    });
+  constructor({ host, user, password, database, port }) {
+    super();
+  }
+
+  async _createTable(type) {
   }
 
   async list(parameters) {
-    try {
-      return await super.list(parameters);
-    } catch (e) {
-      throw errorHandler(e);
-    }
   }
 
   async insert(parameters) {
-    try {
-      return await super.insert(parameters);
-    } catch (e) {
-      throw errorHandler(e);
-    }
   }
 
   async update(parameters) {
-    try {
-      return await super.update(parameters);
-    } catch (e) {
-      throw errorHandler(e);
-    }
   }
 
   async remove(parameters) {
-    try {
-      return await super.remove(parameters);
-    } catch (e) {
-      throw errorHandler(e);
-    }
   }
 
 };
