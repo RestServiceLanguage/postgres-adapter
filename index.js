38b0db0216c84ffc101d8daacffbf25415de4f50
Moritz Schramm <moritz.schramm@gmx.de>
2019-12-26 12:53:33 +0100

    Create connection

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const errorHandler = require('./lib/errorHandler');
 const { DatabaseAdapter } = require('@restservicelanguage/database');
 const pg = require('pg');
 
@@ -6,6 +5,13 @@ module.exports = class PostgresAdapter extends DatabaseAdapter {
 
   constructor({ host, user, password, database, port }) {
     super();
+    this._pool = new pg.Pool({
+      user,
+      host,
+      password,
+      database,
+      port
+    });
   }
 
   async _createTable(type) {
@@ -21,6 +27,23 @@ module.exports = class PostgresAdapter extends DatabaseAdapter {
   }
 
   async remove(parameters) {
+  }
+
+  async _inTransaction(exec) {
+    const client = await this._getConnection();
+    try {
+      await exec(client);
+    } catch (e) {
+      console.error(e);
+      await client.query('ROLLBACK');
+      throw e;
+    } finally {
+      client.release();
+    }
+  }
+
+  async _getConnection() {
+    return await this._pool.connect();
   }
 
 };
