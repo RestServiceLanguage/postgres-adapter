50d560b2cfc95d4d0adcd9313447b8cfb1e827ef
Moritz Schramm <moritz.schramm@gmx.de>
2019-12-27 18:24:05 +0100

    Refactor create table query generation

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-const _ = require('lodash');
 const { DatabaseAdapter } = require('@restservicelanguage/database');
 const pg = require('pg');
-
-const typeMapping = {
-  'Integer': 'INTEGER',
-  'String': 'VARCHAR(255)',
-  'Text': 'TEXT',
-  'Date': 'DATE',
-  'Float': 'DOUBLE PRECISION',
-  'Boolean': 'BOOLEAN'
-};
 
 module.exports = class PostgresAdapter extends DatabaseAdapter {
 
@@ -32,33 +22,6 @@ module.exports = class PostgresAdapter extends DatabaseAdapter {
   }
 
   async _createTable(type) {
-    const columns = _.filter(type.properties, (property) => !_.isArray(property.type));
-    const arrayTables = _.filter(type.properties, (property) => _.isArray(property.type));
-
-    const columnQuery = _.map(columns, (column) => {
-      const customType = _.isNil(typeMapping[column.type]);
-
-      const type = customType ? 'INTEGER' : typeMapping[column.type];
-      let query = `${column.name} ${type}`;
-
-      if (column.uniq) {
-        query += ' UNIQUE';
-      }
-
-      if (!column.nullable) {
-        query += ' NOT NULL';
-      }
-
-      if (customType) {
-        query += `REFERENCES ${column.type.name}(id)`;
-      }
-
-      return query;
-    });
-
-    const baseTableQuery = `CREATE TABLE "${type.name}" (${['id SERIAL PRIMARY KEY', ...columnQuery].join(', ')});`;
-    const arrayQueries = _.map(arrayTables, (property) => this._createArrayTableQuery({ type, property }));
-
     await this._inTransaction(async function(client) {
       this.log(baseTableQuery);
       await client.query(baseTableQuery);
@@ -67,17 +30,6 @@ module.exports = class PostgresAdapter extends DatabaseAdapter {
         await client.query(arrayQuery);
       }
     });
-  }
-
-  _createArrayTableQuery({ type, property }) {
-    const customType = _.isNil(typeMapping[property.type]);
-    const tableName = `${type.name}_${property.name}`;
-
-    const valueType = customType ? 'INTEGER' : typeMapping[property.type];
-
-    const query = `CREATE TABLE "${tableName}" (id SERIAL PRIMARY KEY, "${type.name}" INTEGER REFERENCES "${type.name}"(id), value ${valueType});`;
-
-    return query;
   }
 
   async list(parameters) {
